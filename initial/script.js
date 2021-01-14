44ed255bc52eb6c0bfd5efdd447f3d1d93f5a8e8
MITHUN KUMAR SONI <mkumarsoni75@gmail.com>
2021-01-14 22:02:18 +0530

    completed with small bug

--- initial/script.js
+++ initial/script.js
@@ -3,11 +3,20 @@
 //Selecting elements 
 const scoreZero = document.querySelector('#score--0');
 const scoreOne = document.getElementById('score--1');
+const curr0El = document.querySelector('#current--0');
+const curr1El = document.querySelector('#current--1');
 
+console.log(curr0El);
 const diceEl = document.querySelector('.dice');
 const btnRoll = document.querySelector('.btn--roll');
 const btnNew = document.querySelector('.btn--new');
 const btnHold = document.querySelector('.btn--hold');
+
+const scores = [0,0];
+
+let currentScore = 0;
+let activePlayer = 0;
+let wonTimes =0;
 
 
 
@@ -22,6 +31,59 @@ btnRoll.addEventListener('click',function(){
 const dice = Math.trunc(Math.random()*6) + 1;
 diceEl.classList.remove('hidden');
 diceEl.src = `dice-${dice}.png`;
+if(dice !== 1){
+    currentScore = Number(
+        document.querySelector(`#current--${activePlayer}`).textContent) + dice;
+    document.querySelector(`#current--${activePlayer}`).textContent = currentScore;
+    if(currentScore >= 50){
+        document.querySelector(`#score--${activePlayer}`).textContent = Number(document.querySelector(`#score--${activePlayer}`).textContent) + 1;
+        document.querySelector(`#current--${activePlayer}`).textContent =0;
+    }
+
+
+}else{
+    document.querySelector(`#current--${activePlayer}`).textContent =0;
+    
+    if(activePlayer === 0){
+        document.querySelector(`#score--${activePlayer}`).textContent = Number(document.querySelector(`#score--${activePlayer}`).textContent) + currentScore;
+
+        document.querySelector(`.player--0`).classList.remove('player--active');
+        document.querySelector(`.player--1`).classList.add('player--active');
+        activePlayer = 1;
+
+    }else{
+        document.querySelector(`.player--1`).classList.remove('player--active');
+        document.querySelector(`.player--0`).classList.add('player--active');
+        document.querySelector(`#score--${activePlayer}`).textContent = Number(document.querySelector(`#score--${activePlayer}`).textContent) + currentScore;
+        activePlayer = 0;
+    }
+    currentScore =0;
+   
+}});
+btnHold.addEventListener('click', function(){
+    if(activePlayer === 0){
+        document.querySelector(`.player--0`).classList.remove('player--active');
+        document.querySelector(`.player--1`).classList.add('player--active');
+        activePlayer = 1;
+
+    }else{
+        document.querySelector(`.player--1`).classList.remove('player--active');
+        document.querySelector(`.player--0`).classList.add('player--active');
+        activePlayer = 0;
+    }
+
+
+
+});
+btnNew.addEventListener('click',function(){
+    document.querySelector(`.player--0`).classList.add('player--active');
+    document.querySelector(`.player--1`).classList.remove('player--active');
+    document.querySelector(`#score--0`).textContent = 0;
+    document.querySelector(`#score--1`).textContent = 0;
+    document.querySelector(`#current--0`).textContent =0;
+    document.querySelector(`#current--1`).textContent =0;
+    diceEl.classList.add('hidden');
+
 
 
 
